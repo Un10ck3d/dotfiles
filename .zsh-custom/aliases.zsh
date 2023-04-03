7e2731c7f5a43cfe88b937ae56fb64598f8ee9db
Jonathan Bangert <jonathan@bangert.dk>
2023-04-03 22:00:17 +0200

    Fix config-update command / typo

--- .zsh-custom/aliases.zsh
+++ .zsh-custom/aliases.zsh
@@ -15,4 +15,4 @@ alias i3-config="nvim ~/.config/i3/config"
 
 # Dotfiles
 alias config='/usr/bin/git --git-dir=/home/jonathan/.cfg/ --work-tree=/home/jonathan'
-alias config-update='config add -u && config commit && config pull && config pull'
+alias config-update='config add -u && config commit && config pull && config push'
