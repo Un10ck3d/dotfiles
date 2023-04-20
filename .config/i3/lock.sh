23fc81b2c59512a2656254b647512150ead56d09
Jonathan Bangert <jonathan@bangert.dk>
2023-04-20 08:32:37 +0200

    Better polybar

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -2,6 +2,13 @@
 
 /bin/python $HOME/Code/off.py
 
-i3lock --nofork --line-color='#00000000' --separator-color='#00000000' --clock --insidewrong-color='#00000000' --insidever-color='#00000000' --inside-color='#00000000' -i /home/jonathan/Documents/background-lock-multimonitor.png --indicator
+MONITORS=$(DISPLAY=:0 xrandr -q | grep ' connected' | wc -l)
+
+if (MONITORS == 2)
+then
+	i3lock --nofork --line-color='#00000000' --separator-color='#00000000' --clock --insidewrong-color='#00000000' --insidever-color='#00000000' --inside-color='#00000000' -i /home/jonathan/Documents/background-lock-multimonitor.png --indicator
+else
+	i3lock --nofork --line-color='#00000000' --separator-color='#00000000' --clock --insidewrong-color='#00000000' --insidever-color='#00000000' --inside-color='#00000000' -i /home/jonathan/Documents/background-lock-smol.png --indicator	
+fi
 
 /bin/python $HOME/Code/on.py
